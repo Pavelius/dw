8d3718d1552c508080e1f50fd6977e699391ac44
pavelius <p000000000001@gmail.com>
2018-02-02 17:05:18 +0200

    Листок персонажа

--- dw/hero_sheet.cpp
+++ dw/hero_sheet.cpp
@@ -1,7 +1,24 @@
 #include "main.h"
 
 void hero::sheet() {
-	logs::add("###%1 %2 (%3i уровень)", getname(), getstr(type), level);
+	logs::add("###%1 - %5 %4 %2 (%3i уровень)\n", getname(), getstr(type), level, getstr(race), getstr(alignment));
+	for(auto m = Strenght; m <= Charisma; m = (stat_s)(m + 1)) {
+		if(m != Strenght)
+			logs::add(", ");
+		logs::add("%1 %2i", getstr(m), getraw(m));
+	}
+	logs::add(".\n");
+	for(auto& e : gear) {
+		if(!e)
+			continue;
+		if(&e==gear)
+			logs::add("**В рюкзаке**: ");
+		else
+			logs::add(", ");
+		char temp[260];
+		logs::add(e.getname(temp, true));
+	}
+	logs::add(".\n");
 	logs::add(1, "Закончить просмотр.");
 	auto id = whatdo();
 	switch(id) {
