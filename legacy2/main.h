510212663a08a654408a53730c39e8d78811e9ec
Pavel Chistyakov <p000000000001@gmail.com>
2020-04-05 00:03:24 +0300

    Базовая структура

--- legacy2/main.h
+++ legacy2/main.h
@@ -5,12 +5,54 @@
 
 using namespace logs;
 
-enum distance_s : unsigned char {
-	Hand, Close, Reach, Near, Far,
-};
 enum famlity_stat_s : unsigned char {
 	Reach, Grasp, Sleight,
-	Mood, Tech, Data,
 };
 enum stat_s : unsigned char {
+	Force, Sway, Steel, Lore,
+};
+enum expendable_s : unsigned char {
+	Mood, Tech, Data,
+};
+enum tone_s : unsigned char {
+	Ruins, Echoes, Mirrors,
+};
+enum move_s : unsigned char {
+	HoldTogether, ConductDiplomacy, ClaimByForce, Subterfuge,
+	FlushWithResources, FallIntoCrisis,
+	LendAid, CallInADebt, PowerUp, UncoverSecrets,
+	DefuseForce, DefuseSway, DefuseSteel, DefuseLore,
+	UnleashPower, FiercelyAssault, WastelandSurvival, FindCommonGround,
+	CallForAid, InformationIsPower,
+	FamiliarFace, ForgeAPath, UnearthForgottenLore,
+	ShakeItOff, ProffessionalCare, ToolUp,
+	UnderOrder,
+};
+enum resource_s : unsigned char {
+	Artisans, BarterGoods, Crops, Defences, Energy,
+	Engineering, FreshWater, Justice, Land, Leadership,
+	Luxury, Medicine, Morale, Prestige, Progress,
+	RareMaterials, Recruits, Safety, Scavengers, Scouts,
+	Spies, Trade, Transport, Weaponry,
+};
+enum gender_s : unsigned char {
+};
+typedef cflags<resource_s> resourcea;
+class family {
+	char			stats[Sleight + 1];
+	char			stats_base[Sleight + 1];
+	char			expendable[Data + 1];
+public:
+	int				get(stat_s i) const { return stats[i]; }
+	int				get(expendable_s i) const { return expendable[i]; }
+};
+class nameable {
+	unsigned short	name;
+public:
+	const char*		getname() const;
+};
+class character : public nameable {
+	char			stats[Sway + 1];
+public:
+	int				get(stat_s i) const { return stats[i]; }
 };
